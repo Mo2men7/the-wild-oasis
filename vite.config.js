5d3a0e48de0333906363e48f66cc64e6a113a76d
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-24 11:51:07 +0300

    warning the unsigned values and fixing the prop types problem

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      emitWarning: true,
+      emitError: false,
+    }),
+  ],
 });
