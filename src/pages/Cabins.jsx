656eadfb32f3b09e7ce1a1b9d22456e0ca40715d
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-25 22:53:41 +0300

    Dealing with react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,14 @@
 // import { useEffect } from "react";
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 // import { getCabins } from "../services/apiCobins";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   // useEffect(function () {
   //   getCabins().then((data) => {
   //     console.log(data);
@@ -19,6 +23,8 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
