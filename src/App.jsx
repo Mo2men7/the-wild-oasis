823c36a03f4c2cdf6c39c501a95a4dfb2e9f820c
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-25 04:05:59 +0300

    Building more reusable component with default props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
   background: #eee;
@@ -14,11 +15,24 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Form</Heading>
-        <Heading as="h3">Check in and out</Heading>
-        <Input type="number" placeholder="nUuUmber" />
-        <Button onClick={() => alert("lol")}>LOL</Button>
+        <Row>
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Form</Heading>
+            <Heading as="h3">Check in and out</Heading>
+            <form>
+              <Input type="number" placeholder="nUuUmber" />
+              <Button onClick={() => alert("lol")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("lol")}
+              >
+                Check out
+              </Button>
+            </form>
+          </div>
+        </Row>
       </StyledApp>
     </>
   );
