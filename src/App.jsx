9b69113dbd91d9ac9cb5c8f7499ed80aebfe90d2
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-25 03:32:14 +0300

    Dealing with styled components and define some global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: purple;
+`;
+
+const StyledApp = styled.main`
+  background: #eee;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello, World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Input type="number" placeholder="nUuUmber" />
+        <Button onClick={() => alert("lol")}>LOL</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
