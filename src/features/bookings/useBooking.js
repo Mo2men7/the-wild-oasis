1527a9a9648e85a34965cdd4d9528d0d6ab5c86f
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-28 10:41:46 +0300

    Building the Single Booking Page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+  //   console.log(bookingId);
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(bookingId),
+    retry: false, //to not try to fetch the data more than one time
+  });
+
+  return { isLoading, error, booking };
+}
