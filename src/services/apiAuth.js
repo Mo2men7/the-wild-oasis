ca8cef8046143cdb215dad6d8d10e825d2f52fe1
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-31 12:16:20 +0300

    Simple import edit

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase from "./Supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
